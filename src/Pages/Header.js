bd981cafd5f90220892593c7c0815047f46bc6e9
moshiur25524 <moshiur.rahman25524@gmail.com>
2023-07-08 13:34:33 +0600

    made active link in header component

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -1,43 +1,102 @@
-import React from 'react';
+import React from 'react'
 import './CSS/Header.css'
+import { useState } from 'react'
 
 const Header = () => {
-    return (
-        <div>
-            <nav class="navbar navbar-expand-lg bg-dark fixed-top" data-bs-theme="dark">
-                <div class="container">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <a class="navbar-brand" href="#"><img src="../assets/Resume/Moshiur_Logo_2.png" alt="" /> Moshiur Rahman</a>
-                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                {/* <a class="nav-link active" aria-current="page" href="#">Home</a> */}
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#about">About Me</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#services">Languages</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#portfolio">Projects</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#skills">skills</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#contact">Contact</a>
-                            </li>
-                          
-                        </ul>
-                       
-                    </div>
-                </div>
-            </nav>
-        </div>
-    );
-};
+  const [activeLink, setActiveLink] = useState('home')
 
-export default Header;
+  const handleLinkClick = (link) => {
+    setActiveLink(link)
+  }
+
+  return (
+    <div>
+      <nav
+        class="navbar navbar-expand-lg bg-dark fixed-top"
+        data-bs-theme="dark"
+      >
+        <div class="container">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">
+              <img src="../assets/Resume/Moshiur_Logo_2.png" alt="" /> Moshiur
+              Rahman
+            </a>
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                {/* <a class="nav-link active" aria-current="page" href="#">Home</a> */}
+              </li>
+              <li class="nav-item">
+                <a
+                  className={`nav-link ${
+                    activeLink === 'about' ? 'active' : ''
+                  }`}
+                  onClick={() => handleLinkClick('about')}
+                  href="#about"
+                >
+                  About Me
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  className={`nav-link ${
+                    activeLink === 'services' ? 'active' : ''
+                  }`}
+                  onClick={() => handleLinkClick('services')}
+                  href="#services"
+                >
+                  Language
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  className={`nav-link ${
+                    activeLink === 'portfolio' ? 'active' : ''
+                  }`}
+                  onClick={() => handleLinkClick('portfolio')}
+                  href="#portfolio"
+                >
+                  Projects
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  className={`nav-link ${
+                    activeLink === 'skills' ? 'active' : ''
+                  }`}
+                  onClick={() => handleLinkClick('skills')}
+                  href="#skills"
+                >
+                  Skills
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  className={`nav-link ${
+                    activeLink === 'contact' ? 'active' : ''
+                  }`}
+                  onClick={() => handleLinkClick('contact')}
+                  href="#contact"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,95 +1,163 @@
-import React, { useRef } from 'react';
-import {motion} from 'framer-motion'
+import React, { useRef } from 'react'
+import { motion } from 'framer-motion'
 
 const Projects = () => {
-    const scrollRef = useRef(null)
-    return (
-        <div>
-            <section
-            ref={scrollRef} style={{ overflow: "scroll" }}
-            id="portfolio" class="portfolio section-padding">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="section-header text-center pb-5">
-                                <h2 className='fw-bold'>My Projects</h2>
-                                <p className='fs-5'>Enjoiy all the places with luxurious car , Keep body fit and read more books</p>
-                            </div>
-                        </div>
-                    </div>
+  const scrollRef = useRef(null)
+  return (
+    <div>
+      <section
+        ref={scrollRef}
+        style={{ overflow: 'scroll' }}
+        id="portfolio"
+        class="portfolio section-padding"
+      >
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="section-header text-center pb-5">
+                <h2 className="fw-bold">My Projects</h2>
+                <p className="fs-5">
+                  Enjoiy all the places with luxurious car , Keep body fit and
+                  read more books
+                </p>
+              </div>
+            </div>
+          </div>
 
-                    <div class="row">
-                        <motion.div
-                         class="col-12 col-md-12 col-lg-4"
-                         initial={{ opacity: 0 }}
-                         whileInView={{ opacity: 1 }}
-                         transition={{duration: 2}}
-                         viewport={{ once: true }}
-                         >
-                            <div class="card text-center bg-white pb-2">
-                                <div class="card-body text-dark">
-                                    <div class="img-area mb-4">
-                                        <img src='https://img.freepik.com/free-photo/young-fitness-man-studio_7502-5005.jpg?w=740&t=st=1677061695~exp=1677062295~hmac=2b9fa483b1ecdeed5a6778677035c4e872a4a31abaa7059982fa6b16a00bc951' class="img-fluid" alt="" />
-                                    </div>
-                                    <h3 class="card-title fw-bold">Body Building</h3>
-                                    <p class="lead">Body buiding is a react responsive web application. Login and logOut authentication system is added with firease. As Css framwork, Bootstrap is implmented on it.</p>
-                                    <div>
-                                        <a class="btn bg-success text-white" target='_blank' href="https://body-building-4bc2b.web.app/" rel="noreferrer">Live site</a>
-                                        <a class="btn bg-warning text-dark ms-2" target='_blank' href="https://github.com/moshiur25524/body_building" rel="noreferrer">Github</a>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </motion.div>
-                        <motion.div
-                         class="col-12 col-md-12 col-lg-4"
-                         initial={{ opacity: 0 }}
-                         whileInView={{ opacity: 1 }}
-                         transition={{duration: 3}}
-                         viewport={{ once: true }}
-                         >
-                            <div class="card text-center bg-white pb-2">
-                                <div class="card-body text-dark">
-                                    <div class="img-area mb-4">
-                                        <img src='https://img.freepik.com/free-psd/silver-sedan-car_53876-84522.jpg?w=740&t=st=1677060641~exp=1677061241~hmac=fbd5cd453f1e95902c9a25e8197ba811817025404ccf1df7c800ab50d5f458da' class="img-fluid" alt="" />
-                                    </div>
-                                    <h3 class="card-title fw-bold">Car House</h3>
-                                    <p class="lead">Body buiding is a react web application. some different kinds of charts are added in the dashboard of the project.</p>
-                                    <div>
-                                        <a class="btn bg-success text-white" target='_blank' href="https://gleeful-strudel-be6717.netlify.app/" rel="noreferrer">Live site</a>
-                                        <a class="btn bg-warning text-dark ms-2" target='_blank' href="https://github.com/moshiur25524/Car_House" rel="noreferrer">Github</a>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </motion.div>
-                        <motion.div
-                         class="col-12 col-md-12 col-lg-4"
-                         initial={{ opacity: 0 }}
-                         whileInView={{ opacity: 1 }}
-                         transition={{duration: 4}}
-                         viewport={{ once: true }}
-                         >
-                            <div class="card text-center bg-white pb-2">
-                                <div class="card-body text-dark">
-                                    <div class="img-area mb-4">
-                                        <img src='https://img.freepik.com/free-psd/book-hardcover-mockup_125540-225.jpg?w=740&t=st=1677061375~exp=1677061975~hmac=b0377db0c972d6d8d4424db65cdf06d15f4213830136bffb26c2041043167b6e' class="img-fluid" alt="" />
-                                    </div>
-                                    <h3 class="card-title fw-bold">Bookory</h3>
-                                    <p class="lead">Book shop is a full stack web application. React is implemented as a library of javaScript. Besides, Node.js, Mongodb also implented.</p>
-                                    <div>
-                                        <a class="btn bg-success text-white" target='_blank' href="https://bookory-cc089.web.app/" rel="noreferrer">Live site</a>
-                                        <a class="btn bg-warning text-dark ms-2" target='_blank' href="https://github.com/moshiur25524/Bookory_Client" rel="noreferrer">Github</a>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </motion.div>
-                    </div>
+          <div class="row">
+            <motion.div
+              class="col-12 col-md-12 col-lg-4"
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 2 }}
+              viewport={{ once: true }}
+            >
+              <div class="card text-center bg-white pb-2">
+                <div class="card-body text-dark">
+                  <div class="img-area mb-4">
+                    <img
+                      src="https://img.freepik.com/free-photo/young-fitness-man-studio_7502-5005.jpg?w=740&t=st=1677061695~exp=1677062295~hmac=2b9fa483b1ecdeed5a6778677035c4e872a4a31abaa7059982fa6b16a00bc951"
+                      class="img-fluid"
+                      alt=""
+                    />
+                  </div>
+                  <h3 class="card-title fw-bold">Body Building</h3>
+                  <p class="lead">
+                    Body buiding is a react responsive web application. Login
+                    and logOut authentication system is added with firease. As
+                    Css framwork, Bootstrap is implmented on it.
+                  </p>
+                  <div>
+                    <a
+                      class="btn bg-success text-white"
+                      target="_blank"
+                      href="https://body-building-4bc2b.firebaseapp.com/"
+                      rel="noreferrer"
+                    >
+                      Live site
+                    </a>
+                    <a
+                      class="btn bg-warning text-dark ms-2"
+                      target="_blank"
+                      href="https://github.com/moshiur25524/body_building"
+                      rel="noreferrer"
+                    >
+                      Github
+                    </a>
+                  </div>
                 </div>
-            </section>
+              </div>
+            </motion.div>
+            <motion.div
+              class="col-12 col-md-12 col-lg-4"
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 3 }}
+              viewport={{ once: true }}
+            >
+              <div class="card text-center bg-white pb-2">
+                <div class="card-body text-dark">
+                  <div class="img-area mb-4">
+                    <img
+                      src="https://img.freepik.com/free-psd/silver-sedan-car_53876-84522.jpg?w=740&t=st=1677060641~exp=1677061241~hmac=fbd5cd453f1e95902c9a25e8197ba811817025404ccf1df7c800ab50d5f458da"
+                      class="img-fluid"
+                      alt=""
+                    />
+                  </div>
+                  <h3 class="card-title fw-bold">Car House</h3>
+                  <p class="lead">
+                    Body buiding is a react web application. some different
+                    kinds of charts are added in the dashboard of the project.
+                  </p>
+                  <div>
+                    <a
+                      class="btn bg-success text-white"
+                      target="_blank"
+                      href="https://gleeful-strudel-be6717.netlify.app/"
+                      rel="noreferrer"
+                    >
+                      Live site
+                    </a>
+                    <a
+                      class="btn bg-warning text-dark ms-2"
+                      target="_blank"
+                      href="https://github.com/moshiur25524/Car_House"
+                      rel="noreferrer"
+                    >
+                      Github
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </motion.div>
+            <motion.div
+              class="col-12 col-md-12 col-lg-4"
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 4 }}
+              viewport={{ once: true }}
+            >
+              <div class="card text-center bg-white pb-2">
+                <div class="card-body text-dark">
+                  <div class="img-area mb-4">
+                    <img
+                      src="https://img.freepik.com/free-psd/book-hardcover-mockup_125540-225.jpg?w=740&t=st=1677061375~exp=1677061975~hmac=b0377db0c972d6d8d4424db65cdf06d15f4213830136bffb26c2041043167b6e"
+                      class="img-fluid"
+                      alt=""
+                    />
+                  </div>
+                  <h3 class="card-title fw-bold">Bookory</h3>
+                  <p class="lead">
+                    Book shop is a full stack web application. React is
+                    implemented as a library of javaScript. Besides, Node.js,
+                    Mongodb also implented.
+                  </p>
+                  <div>
+                    <a
+                      class="btn bg-success text-white"
+                      target="_blank"
+                      href="https://bookory-cc089.web.app/"
+                      rel="noreferrer"
+                    >
+                      Live site
+                    </a>
+                    <a
+                      class="btn bg-warning text-dark ms-2"
+                      target="_blank"
+                      href="https://github.com/moshiur25524/Bookory_Client"
+                      rel="noreferrer"
+                    >
+                      Github
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </motion.div>
+          </div>
         </div>
-    );
-};
+      </section>
+    </div>
+  )
+}
 
-export default Projects;
+export default Projects
